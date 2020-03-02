b9988341d515251f7ae8441cc2915861fe683a16
Rajat <rajat@Rajats-MBP.local>
2020-03-02 21:21:53 +0100

    asss

---
+++ hello.py
@@ -0,0 +1,2 @@
+from domain_model6 import rajat
+print('rajat', rajat)
--- submodules.sh
+++ submodules.sh
@@ -1,37 +1,19 @@
 #!/bin/bash
-
-echo Hello, World!
-
-DIR="./attach7"
+echo "Arg 0: $1"
+GITHUB_TOKEN=6228a2e824c45f4ea10db728913398786aaec7b0
+echo $GITHUB_TOKEN
+DIR="./shared"
 if ! [ -d "$DIR" ]; then
   ### Take action if $DIR exists ###
   echo 'Intializing the submodule'
-  git submodule add git@github.com:rajathy2020/second_repo.git attach7
-  git submodule init
-  git submodule update
-  cd attach7
-  git checkout master
-  echo 'Getting the latest submodule update'
-  git pull
-  cd ..
-  git add attach7
-  git add submodles.sh
-  git commit -m 'updating repo with latest submodule'
-  git push
-  echo "Updates pushed to remote"
-else
+  git submodule add https://$GITHUB_TOKEN@github.com/rajathy2020/second_repo.git shared
+ else
   ###  Control will jump here if $DIR does NOT exists ###
   git submodule init
   git submodule update
-  cd attach7
-  git checkout master
+  cd shared
+  git checkout $1
   echo 'Getting the latest submodule update'
   git pull
   cd ..
-  git add attach7
-  git add submodules.sh.sh
-  git commit -m 'updating repo with latest submodule'
-  git push
-  echo "Updates pushed to remote"
 fi
-
